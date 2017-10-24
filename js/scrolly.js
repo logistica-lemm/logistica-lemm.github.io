0a747a32336ab582554b1bd91421421b8694f961
Andrea Gómez <andrea.g1028@gmail.com>
2017-10-23 19:35:30 -0500

    Scroll suave ya no interfiere con carousel, carousel oculto hasta reparación

--- js/scrolly.js
+++ js/scrolly.js
@@ -1,28 +1,36 @@
+// Select all anchors with links
 $('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            return false;
-          } else {
-            $target.attr('tabindex','-1');
-            $target.focus();
-          };
-        });
-      }
-    }
-  });
+
+// Exceptions
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .not('[href="#carouselIndicators"]')
+    .click(function(event) {
+        // Links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname
+           ) {
+            // Element to scroll
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate( {
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1');
+                        // Set focus again
+                        $target.focus();
+                    };
+                });
+            }
+        }
+    });
